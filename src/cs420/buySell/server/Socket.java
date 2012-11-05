1a0a495227e0d35bacd8fbfb4e78c07a5c848d02
Kevin Reuter <fastaccord1@gmail.com>
2012-11-05 14:25:27 -0600

    Adding socket code for SSL

--- src/cs420/buySell/server/Socket.java
+++ src/cs420/buySell/server/Socket.java
@@ -4,10 +4,7 @@ import javax.net.ssl.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
+import java.security.*;
 import java.security.cert.CertificateException;
 
 /**
@@ -19,19 +16,27 @@ public class Socket implements Runnable {
     private SSLServerSocket serverSocket;
     private SSLSocket socket;
     private KeyStore keyStore;
-    private KeyManager keyManager;
+    private KeyManager[] keyManagers;
     private SSLContext sslContext;
     private SSLSession sslSession;
     private final int PORT = 25000;
 
     public Socket() throws KeyStoreException, IOException, NoSuchAlgorithmException, CertificateException,
-            UnrecoverableKeyException {
+            UnrecoverableKeyException, KeyManagementException {
         keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
         keyStore.load(new FileInputStream("keystore.ks"), null);
 
         KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
         keyManagerFactory.init(keyStore, null);
 
+        sslContext = SSLContext.getInstance("SSLv3");
+
+        keyManagers = keyManagerFactory.getKeyManagers();
+
+        sslContext.init(keyManagers, null, null);
+
+        socketFactory = sslContext.getServerSocketFactory();
+        serverSocket = (SSLServerSocket)socketFactory.createServerSocket(PORT);
 
     }
 
