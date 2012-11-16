d900c71d21be5795f6460aea1614b58953871b34
Kevin Reuter <fastaccord1@gmail.com>
2012-11-15 20:16:10 -0600

    Added file to handle communications on server side(protocol handling)

---
+++ src/cs420/buySell/server/ServerNetwork.java
@@ -0,0 +1,12 @@
+package cs420.buySell.server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kreuter
+ * Date: 11/15/12
+ * Time: 7:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServerNetwork {
+
+}
