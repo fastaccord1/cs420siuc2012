ca3c6d937444d2fe580b774e5d45e2cbcbb0256b
Kevin Reuter <fastaccord1@gmail.com>
2012-11-15 18:18:55 -0600

    Added file to create socket from client to client

---
+++ src/cs420/buySell/client/SocketClientToClient.java
@@ -0,0 +1,11 @@
+package cs420.buySell.client;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kreuter
+ * Date: 11/15/12
+ * Time: 6:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SocketClientToClient {
+}
