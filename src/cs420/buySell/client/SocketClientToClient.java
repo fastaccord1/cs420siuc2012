fecde0e059f1331f757359b548684c3a0190ee32
Kevin Reuter <fastaccord1@gmail.com>
2012-11-15 18:44:35 -0600

    Adding code to handle sending/receiving messages between clients

--- src/cs420/buySell/client/SocketClientToClient.java
+++ src/cs420/buySell/client/SocketClientToClient.java
@@ -1,5 +1,10 @@
 package cs420.buySell.client;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: kreuter
@@ -7,5 +12,43 @@ package cs420.buySell.client;
  * Time: 6:18 PM
  * To change this template use File | Settings | File Templates.
  */
-public class SocketClientToClient {
+public class SocketClientToClient implements Runnable{
+
+    private DatagramSocket socket;
+    private final int PORT = 25001;
+
+    public SocketClientToClient() {
+        try {
+            socket = new DatagramSocket(PORT);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void sendData()
+
+    public void waitForPackets() {
+        while(true) {
+            try{
+                byte[] data = new byte[1024];
+                DatagramPacket received = new DatagramPacket(data, data.length);
+
+
+                socket.receive(received);
+                String output = new String(received.getData());
+
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+
+
+        }
+
+    }
+
+    @Override
+    public void run() {
+        this.waitForPackets();
+    }
 }
