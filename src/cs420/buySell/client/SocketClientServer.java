ee9dced197c5d3984b01abc1ab51fe85a420a8f1
Kevin Reuter <fastaccord1@gmail.com>
2012-11-25 10:25:16 -0600

    Writing code for client-server communication

--- src/cs420/buySell/client/SocketClientServer.java
+++ src/cs420/buySell/client/SocketClientServer.java
@@ -1,5 +1,13 @@
 package cs420.buySell.client;
 
+import javax.net.ssl.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.security.*;
+import java.security.cert.CertificateException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: kreuter
@@ -8,4 +16,34 @@ package cs420.buySell.client;
  * To change this template use File | Settings | File Templates.
  */
 public class SocketClientServer {
+
+    private SSLSocket socket;
+    private SSLSocketFactory socketFactory;
+    private KeyStore keyStore;
+    private KeyManager[] keyManagers;
+    private SSLContext sslContext;
+
+    public SocketClientServer(InetAddress serverAddress, int port) throws KeyStoreException,
+            IOException, NoSuchAlgorithmException, CertificateException, UnrecoverableKeyException,
+            KeyManagementException {
+
+        keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        keyStore.load(new FileInputStream("keystore.ks"), null);
+
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+        keyManagerFactory.init(keyStore, null);
+
+        sslContext = SSLContext.getInstance("SSLv3");
+
+        keyManagers = keyManagerFactory.getKeyManagers();
+
+        sslContext.init(keyManagers, null, null);
+
+        socketFactory = sslContext.getSocketFactory();
+
+        socket = (SSLSocket)socketFactory.createSocket(serverAddress, port);
+
+        socket.startHandshake();
+
+    }
 }
