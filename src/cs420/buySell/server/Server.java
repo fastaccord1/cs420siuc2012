f9cd2beaeb740d0894cdb80666f9d27b93138b36
Kevin Reuter <fastaccord1@gmail.com>
2012-12-12 00:25:23 -0600

    Added comments to the file and fixed warnings

--- src/cs420/buySell/server/Server.java
+++ src/cs420/buySell/server/Server.java
@@ -1,18 +1,18 @@
 package cs420.buySell.server;
 
+import cs420.buySell.client.Client;
+
 import java.io.*;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.LinkedList;
-import cs420.buySell.client.Client;
 
 /**
- * Created with IntelliJ IDEA.
- * User: kevin
- * Date: 12/11/12
- * Time: 7:44 PM
- * To change this template use File | Settings | File Templates.
+ * Server.java
+ *
+ * Buy_sell_exchange
+ * Kevin Reuter, Jordan Martin, and Matt Troutt
+ * This class handles the client-server communication on the server side
  */
 
 public class Server {
@@ -24,6 +24,10 @@ public class Server {
     private static final int MAX_CONNECTIONS = 100;
     private static LinkedList<Client> clients;
 
+    /**
+     * Main method that starts the server and handles starts a thread for each incoming connection.
+     * @param args Command line arguments(there are none).
+     */
     public static void main(String[] args) {
         clients = new LinkedList<Client>();
         try{
@@ -32,7 +36,7 @@ public class Server {
 
             Socket server;
             int connections = 0;
-            while(connections++ < MAX_CONNECTIONS || MAX_CONNECTIONS == 0){
+            while(connections++ < MAX_CONNECTIONS){
                 Communication comm;
 
                 server = serverSocket.accept();
@@ -47,24 +51,32 @@ public class Server {
         }
     }
 
-
-
-
 }
 
+/**
+ * This class actually contains the socket code for the server
+ */
 class Communication implements Runnable{
     private Socket socket;
-    private String line, input;
+    private String line;
     private LinkedList<Client> clients;
 
+    /**
+     * Constructor that initializes the variables for Communication class
+     * @param socket The socket object created by the main method
+     * @param clients A LinkedList of connected clients
+     */
     Communication(Socket socket, LinkedList<Client> clients){
         this.socket = socket;
         this.clients = clients;
 
     }
 
+    /**
+     * This method overrides the run method from Runnable to allow for multiple threads to be run.
+     * It waits for incoming messages and sends those messages to an interpreter method for processing.
+     */
     public void run(){
-        input = "";
         try{
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintStream out = new PrintStream(socket.getOutputStream());
@@ -90,6 +102,11 @@ class Communication implements Runnable{
 
     }
 
+    /**
+     * Interpreter method that will interpret the message and call the appropriate method
+     * @param input    The message to be interpreted.
+     * @throws IOException Throws this exception if sendList throws the exception.
+     */
     public void interpret(String input) throws IOException {
         if(input.equals("get_list")){
             sendList();
@@ -97,6 +114,10 @@ class Communication implements Runnable{
 
     }
 
+    /**
+     * This method sends the list of connected clients to the client that requested it.
+     * @throws IOException Throws this if PrintWriter cannot be initialized.
+     */
     public void sendList() throws IOException {
         String output = "list_incoming";
         PrintWriter out = new PrintWriter(socket.getOutputStream());
